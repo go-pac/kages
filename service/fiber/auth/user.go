dcb6af84583019a6dc0c79c1e4fd4e71e8a16a8f
Bogdan Ciuca <28389201+bogdanciuca@users.noreply.github.com>
2022-05-22 00:37:45 +0300

    Add fiber auth user helper

---
+++ service/fiber/auth/user.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/go-pac/kages/model"
+)
+
+// todo: base64 decode ID
+func GetUserFromCtx(ctx *fiber.Ctx) *model.User {
+	if userData := ctx.Locals(UserKey); userData != nil {
+		if user, ok := userData.(*model.User); ok {
+			return user
+		}
+	}
+
+	return nil
+}
+
+func GetTokenFromCtx(ctx *fiber.Ctx) string {
+	if tokenVal := ctx.Locals(TokenKey); tokenVal != nil {
+		if token, ok := tokenVal.(string); ok {
+			return token
+		}
+	}
+
+	return ""
+}
